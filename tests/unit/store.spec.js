18d53eb350f0af98764f3f3c748685efd6b10ef6
Aamaruvi Yogamani <38222826+Technical27@users.noreply.github.com>
2020-01-04 12:27:34 -0500

    trimmed whitespace

--- tests/unit/store.spec.js
+++ tests/unit/store.spec.js
@@ -7,9 +7,9 @@ describe('state mutations', () => {
   describe('createCard', () => {
     it('can create a new card', () => {
       const state = {cards: []};
-     
+
       createCard(state, {id: 1});
-      
+
       expect(state.cards.length).to.equal(1);
       expect(state.cards[0].name).to.equal('test');
       expect(state.cards[0].body).to.equal('test');
@@ -29,9 +29,9 @@ describe('state mutations', () => {
   describe('changeName', () => {
     it('can change the name of a card', () => {
       const state = {cards: [{id: 1, name: 'test', body: 'test'}]};
-     
+
       changeName(state, {newName: 'name', id: 1});
-      
+
       expect(state.cards[0].name).to.equal('name');
     });
   });
@@ -39,9 +39,9 @@ describe('state mutations', () => {
   describe('changeBody', () => {
     it('can change the body of a card', () => {
       const state = {cards: [{id: 1, name: 'test', body: 'test'}]};
-     
+
       changeBody(state, {newBody: 'body', id: 1});
-      
+
       expect(state.cards[0].body).to.equal('body');
     });
   });
